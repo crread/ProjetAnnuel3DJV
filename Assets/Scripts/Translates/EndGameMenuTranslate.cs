5fb670e045ec1b0c35419a5d7e08513918434316
unknown <mickaelberthelot@outlook.fr>
2021-07-28 22:31:41 +0200

    save

---
+++ Assets/Scripts/Translates/EndGameMenuTranslate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Translates
+{
+    public class EndGameMenuTranslate : MonoBehaviour
+    {
+        public GameObject winTitle;
+        public GameObject loseTitle;
+        public GameObject replayButtonLose;
+        public GameObject quitButtonLose;
+        public GameObject replayButtonWin;
+        public GameObject quitButtonWin;
+        
+        private DDOL _ddol;
+        
+        private void Start()
+        {
+            _ddol = GameObject.Find("Preload").GetComponent<DDOL>();
+            
+            winTitle.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.win.title"];
+            loseTitle.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.lose.title"];
+            replayButtonLose.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.restart"];
+            quitButtonLose.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.quit"];
+            replayButtonWin.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.restart"];
+            quitButtonWin.GetComponent<Text>().text = _ddol.translates.translation["game.endGame.quit"];
+        }
+    }
+}
---
+++ Assets/Scripts/Translates/GameTranslate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Translates
+{
+    public class GameTranslate : MonoBehaviour
+    {
+        public GameObject QuitButton;
+        public GameObject RestartButton;
+        public GameObject ContinuButton;
+
+        private DDOL _ddol;
+        void Start()
+        {
+            _ddol = GameObject.Find("Preload").GetComponent<DDOL>();
+            
+            QuitButton.GetComponent<Text>().text = _ddol.translates.translation["game.menu.keepPlaying"];
+            RestartButton.GetComponent<Text>().text = _ddol.translates.translation["game.menu.restartGame"];
+            ContinuButton.GetComponent<Text>().text = _ddol.translates.translation["game.menu.quitGame"];
+        }
+    }
+}
